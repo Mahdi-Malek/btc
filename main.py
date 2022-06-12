f9772add18a4d699c02db074b4e9c2ce6083bed7
مهدی حاجیان <m.hajian1@pjpd.ir>
2022-06-12 17:13:13 +0430

    Signal simulation added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import tti.indicators as tti
 
-eth_df = pd.read_csv("D:\\Data\\eth-usdt.csv")
+eth_df = pd.read_csv("file path")
+
 
 # ------------------ On Balance Volume Index ------------------
 OBV_df = eth_df[["Open Time","Close","Volume"]]
@@ -15,3 +16,9 @@ RSI_df = eth_df
 RSI_df.index = pd.to_datetime(RSI_df["Open Time"],format="%Y-%m-%d %H:%M:%S")
 RSI_Ind = tti.RelativeStrengthIndex(input_data=RSI_df)
 #RSI_Ind.getTiGraph().show()
+simulation_data, simulation_statistics, simulation_graph = RSI_Ind.getTiSimulation(
+    close_values=RSI_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)
+simulation_data.to_csv("file path")
+
+
