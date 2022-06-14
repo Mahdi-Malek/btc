8f70dea234041209b0bb8b5857c2d890c43eea00
مهدی حاجیان <m.hajian1@pjpd.ir>
2022-06-14 16:00:55 +0430

    Add new index and its signals

--- main.py
+++ main.py
@@ -1,24 +1,68 @@
 import pandas as pd
 import tti.indicators as tti
 
-eth_df = pd.read_csv("file path")
-
+eth_df = pd.read_csv("Your Path")
+eth_df.index = pd.to_datetime(eth_df["Open Time"],format="%Y-%m-%d %H:%M:%S")
 
 # ------------------ On Balance Volume Index ------------------
-OBV_df = eth_df[["Open Time","Close","Volume"]]
-OBV_df.index = pd.to_datetime(OBV_df["Open Time"],format="%Y-%m-%d %H:%M:%S")
-OBV_Ind = tti.OnBalanceVolume(input_data=OBV_df)
+OBV_Ind = tti.OnBalanceVolume(input_data=eth_df)
 #OBV_Ind.getTiGraph().show()
-
+simulation_data, simulation_statistics, simulation_graph = OBV_Ind.getTiSimulation(
+    close_values=eth_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
 
 # ------------------ Relative Strenght Index ------------------
-RSI_df = eth_df
-RSI_df.index = pd.to_datetime(RSI_df["Open Time"],format="%Y-%m-%d %H:%M:%S")
-RSI_Ind = tti.RelativeStrengthIndex(input_data=RSI_df)
+RSI_Ind = tti.RelativeStrengthIndex(input_data=eth_df)
 #RSI_Ind.getTiGraph().show()
 simulation_data, simulation_statistics, simulation_graph = RSI_Ind.getTiSimulation(
-    close_values=RSI_df[['close']], max_exposure=None,
+    close_values=eth_df[['close']], max_exposure=None,
     short_exposure_factor=1.5)
-simulation_data.to_csv("file path")
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
 
+# ------------------ Bollinger Bands Index ------------------
+BBI_Ind = tti.BollingerBands(input_data=eth_df)
+#BBI_Ind.getTiGraph().show()
+simulation_data, simulation_statistics, simulation_graph = BBI_Ind.getTiSimulation(
+    close_values=eth_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
 
+# ------------------ Moving Average Convergence Divergence ------------------
+MACD_Ind = tti.MovingAverageConvergenceDivergence(input_data=eth_df)
+#MACD_Ind.getTiGraph().show()
+simulation_data, simulation_statistics, simulation_graph = MACD_Ind.getTiSimulation(
+    close_values=eth_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)#print('\nSimulation Data:\n', simulation_data)
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
+
+# ------------------ Time Series Forecast Index ------------------
+TSF_Ind = tti.TimeSeriesForecast(input_data=eth_df)
+#TSF_Ind.getTiGraph().show()
+simulation_data, simulation_statistics, simulation_graph = TSF_Ind.getTiSimulation(
+    close_values=eth_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
+
+# ------------------ Relative Volatility Index ------------------
+RVI_Ind = tti.RelativeVolatilityIndex(input_data=eth_df)
+#TSF_Ind.getTiGraph().show()
+simulation_data, simulation_statistics, simulation_graph = RVI_Ind.getTiSimulation(
+    close_values=eth_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
+
+# ------------------ Fibonacci Retracement Index ------------------
+Fib_Ind = tti.FibonacciRetracement(input_data=eth_df)
+#TSF_Ind.getTiGraph().show()
+simulation_data, simulation_statistics, simulation_graph = Fib_Ind.getTiSimulation(
+    close_values=eth_df[['close']], max_exposure=None,
+    short_exposure_factor=1.5)
+#print('\nSimulation Data:\n', simulation_data)
+simulation_data.to_csv("Your Path")
